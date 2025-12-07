f82610969a8bd8d292de61ab645eacd27d9f7f0a
Aktan Azat <aazat@ucdavis.edu>
2025-12-07 03:26:00 -0800

    streamline png harness by dropping signature check

--- part-a-harness/png_harness.c
+++ part-a-harness/png_harness.c
@@ -6,10 +6,9 @@
 int main(int argc, char **argv) {
     if (argc < 3) return 1;
 
-    const char *inpath = argv[1];
-    const char *outpath = argv[2];
+    FILE *fp = fopen(argv[1], "rb");
+    char* outfile = argv[2];
 
-    FILE *fp = fopen(inpath, "rb");
     if (fp == NULL) return 1;
 
     png_structp png = png_create_read_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
@@ -18,6 +17,9 @@ int main(int argc, char **argv) {
         return 1;
     }
 
+    /* Provide I/O */
+    png_init_io(png, fp);
+
     png_infop info = png_create_info_struct(png);
     if (info == NULL) {
         png_destroy_read_struct(&png, NULL, NULL);
@@ -25,65 +27,34 @@ int main(int argc, char **argv) {
         return 1;
     }
 
-    if (setjmp(png_jmpbuf(png))) {
-        png_destroy_read_struct(&png, &info, NULL);
-        fclose(fp);
-        return 1;
-    }
-
-    unsigned char sig[8];
-    if (fread(sig, 1, 8, fp) != 8) {
-        png_destroy_read_struct(&png, &info, NULL);
-        fclose(fp);
-        return 1;
-    }
-    if (png_sig_cmp(sig, 0, 8)) {
-        png_destroy_read_struct(&png, &info, NULL);
-        fclose(fp);
-        return 1;
-    }
-
-    png_init_io(png, fp);
-    png_set_sig_bytes(png, 8);
-
     png_read_info(png, info);
-
     png_set_expand(png);
     png_set_gray_to_rgb(png);
     png_set_palette_to_rgb(png);
     png_set_filler(png, 0xFF, PNG_FILLER_AFTER);
     png_set_scale_16(png);
     png_set_packing(png);
-
-    png_uint_32 width, height;
-    int bit_depth, color_type, interlace_method, compression_method, filter_method;
-    if (!png_get_IHDR(png, info, &width, &height, &bit_depth, &color_type,
-                      &interlace_method, &compression_method, &filter_method)) {
-        png_destroy_read_struct(&png, &info, NULL);
-        fclose(fp);
-        return 1;
-    }
-
     png_read_update_info(png, info);
+    int channels = png_get_channels(png, info);
+    int colorType = png_get_color_type(png, info);
+    png_size_t rowBytes = png_get_rowbytes(png, info);
+    int width = png_get_image_width(png, info);
+    int height = png_get_image_height(png, info);
 
-    png_size_t rowbytes = png_get_rowbytes(png, info);
-    if (rowbytes == 0 || height == 0) {
-        png_destroy_read_struct(&png, &info, NULL);
-        fclose(fp);
-        return 1;
-    }
-
-    png_bytep *rows = (png_bytep *)malloc(sizeof(png_bytep) * height);
+    png_bytep *rows = malloc(sizeof(png_bytep) * height);
     if (rows == NULL) {
+        perror("Malloc failed for rows");
         png_destroy_read_struct(&png, &info, NULL);
         fclose(fp);
         return 1;
     }
-
-    for (png_uint_32 y = 0; y < height; ++y) {
-        rows[y] = (png_bytep)malloc(rowbytes);
-        if (rows[y] == NULL) {
-            for (png_uint_32 i = 0; i < y; ++i) free(rows[i]);
+    for (int i = 0; i < height; i++) {
+        rows[i] = malloc(rowBytes);
+        if (rows[i] == NULL) {
+            perror("Malloc failed for rows");
+            for (int j = 0; j < i; j++) {
+                free(rows[j]);
+            }
             free(rows);
             png_destroy_read_struct(&png, &info, NULL);
             fclose(fp);
@@ -91,36 +62,47 @@ int main(int argc, char **argv) {
         }
     }
 
-    png_read_image(png, rows);
-    png_read_end(png, info);
-    fclose(fp);
-
-    /// Optional write API
-    FILE *out = fopen(outpath, "wb");
-    if (!out) {
-        for (png_uint_32 y = 0; y < height; ++y) free(rows[y]);
+    if (setjmp(png_jmpbuf(png))) {
+        for (int i = 0; i < height; i++) {
+            free(rows[i]);
+        }
         free(rows);
         png_destroy_read_struct(&png, &info, NULL);
+        fclose(fp);
+        return 1;
+    }
+
+    png_read_image(png, rows);
+    png_read_end(png, info);
+
+    png_destroy_read_struct(&png, &info, NULL);
+    fclose(fp);
+
+    FILE *out = fopen(outfile, "wb");
+    if (out == NULL) {
+        perror("open output");
         return 2;
     }
 
     png_structp wpng = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
     png_infop   winfo = png_create_info_struct(wpng);
-    if (!wpng || !winfo) {
-        fclose(out);
-        if (wpng != NULL) png_destroy_write_struct(&wpng, NULL);
-        for (png_uint_32 y = 0; y < height; ++y) free(rows[y]);
+    if (wpng == NULL || winfo == NULL) {
+        for (int i = 0; i < height; i++) {
+            free(rows[i]);
+        }
         free(rows);
-        png_destroy_read_struct(&png, &info, NULL);
+        png_destroy_write_struct(&wpng, &winfo);
+        fclose(out);
         return 2;
     }
 
     if (setjmp(png_jmpbuf(wpng))) {
-        fclose(out);
-        png_destroy_write_struct(&wpng, &winfo);
-        for (png_uint_32 y = 0; y < height; ++y) free(rows[y]);
+        for (int i = 0; i < height; i++) {
+            free(rows[i]);
+        }
         free(rows);
-        png_destroy_read_struct(&png, &info, NULL);
+        png_destroy_write_struct(&wpng, &winfo);
+        fclose(out);
         return 2;
     }
 
@@ -137,12 +119,14 @@ int main(int argc, char **argv) {
     png_write_image(wpng, rows);
     png_write_end(wpng, winfo);
 
+    for (int i = 0; i < height; i++) {
+        free(rows[i]);
+    }
+
+    free(rows);
+
     png_destroy_write_struct(&wpng, &winfo);
     fclose(out);
-
-    for (png_uint_32 y = 0; y < height; ++y) free(rows[y]);
-    free(rows);
-    png_destroy_read_struct(&png, &info, NULL);
 
     return 0;
 }
